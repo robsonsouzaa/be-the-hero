1baf6643c67c3fbd57898c0bac6473fd355d045c
Robson Souza <robsonex@gmail.com>
2020-03-30 16:12:50 -0400

    alterações back-end

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,12 +1,11 @@
 const connection = require('../database/connection');
 
 module.exports = {
+  
   async index (req, res) {
     const{page = 1} = req.query;
 
     const [count] = await connection('incidents').count();
-
-    console.log(count);
 
     const incidents = await connection('incidents')
       .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
